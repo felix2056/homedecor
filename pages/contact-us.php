510b13dd3eecb9a2fa88dd963f670a285dfff6f6
felix7595 <felixdaniel2056@gmail.com>
2020-09-28 15:24:44 +0100

    test

--- pages/contact-us.php
+++ pages/contact-us.php
@@ -12,7 +12,7 @@
 
             <h2 class="mb-3">Contact Us</h2>
 
-            <form method="post" action="/account" id="create_customer" class="row" accept-charset="UTF-8">
+            <form method="post" action="" id="create_customer" class="row" accept-charset="UTF-8">
                 <div class="col-md-6">
                     <input type="text" value="" name="name" id="name" placeholder="Name"
                         autofocus="">
@@ -29,6 +29,10 @@
 
                 <div class="col-md-12">
                     <textarea name="message" id="message" cols="30" rows="5"></textarea>
+                </div>
+
+                <div class="col-md-4">
+                    <button type="submit" class="contact-btn mt-3">Submit</button>
                 </div>
             </form>
 
